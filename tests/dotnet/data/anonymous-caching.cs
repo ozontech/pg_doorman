854185805c95e34640ca7f2995f3f78791a07c7e
belov_i <belov_i@ews.ru>
2026-01-13 17:09:36 +0500

    anon caching test fix

--- tests/dotnet/data/anonymous-caching.cs
+++ tests/dotnet/data/anonymous-caching.cs
@@ -5,19 +5,19 @@ string connectionString = Environment.GetEnvironmentVariable("DATABASE_URL")
     ?? "Host=127.0.0.1;Port=6433;Database=example_db;User Id=example_user_1;Password=test;SSLMode=Disable";
 
 await using var connection = new NpgsqlConnection(connectionString);
-await using var batch = connection.CreateBatch();
+await using var selectionBatch = connection.CreateBatch();
 
-for (int i = 0; i < 1024; i++)
+for (int i = 0; i < 2; i++)
 {
-    var command = batch.CreateBatchCommand();
-    command.CommandText = "select 1::bigint";
-    batch.BatchCommands.Add(command);
+    var sCommand = selectionBatch.CreateBatchCommand();
+    // Response data should be greater than Server.buffer.len()
+    sCommand.CommandText = "select * from generate_series(1, 10000)";
+    selectionBatch.BatchCommands.Add(sCommand);
 }
-
 await connection.OpenAsync();
 try
 {
-    var reader = await batch.ExecuteReaderAsync();
+    var reader = await selectionBatch.ExecuteReaderAsync();
     do
     {
         while (await reader.ReadAsync())
